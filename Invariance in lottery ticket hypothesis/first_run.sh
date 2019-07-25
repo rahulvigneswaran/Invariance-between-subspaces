5c883f9dec6ed1f96e536d06c5e3962d12f9809f
Rahul-Vigneswaran K <rahulvigneswaran@gmail.com>
2019-07-25 15:13:55 +0530

    Made a separate file for running in server

--- Invariance in lottery ticket hypothesis/first_run.sh
+++ Invariance in lottery ticket hypothesis/first_run.sh
@@ -7,7 +7,7 @@ inp=20
 for (( i=1; i<${arraylength}+1; i++ ));do
 for topk in 5 10 20 30 40 500; do
 for pr in 5 7 10 15; do
-        python Lottery_run.py --layer-sizes ${array[i-1]} --top $topk --input-dim $inp --per $pr
+        python server_lottery_run.py --layer-sizes ${array[i-1]} --top $topk --input-dim $inp --per $pr
 done
 done
 done
